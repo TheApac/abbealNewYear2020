728473c104d60cb13e8ac9aab93878604286b6e6
TheApac <apac.alexcuevas@gmail.com>
2020-12-11 16:20:59 +0100

    Trying to make the app boot

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello, World!'
-	
-app.run(threaded=True, port=5000)
+
+if __name__ == '__main__':
+	app.run(threaded=True, port=5000)
